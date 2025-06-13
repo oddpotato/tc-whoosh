a95a3d88b3f1a7f37122eaee37668b7080a6bf2d
Fin Apps <wikipanyd@gmail.com>
2025-06-13 12:11:56 +0100

    I will be damned if I let docker fill my harddrive with nonsense again. Adding cursed code to the deploy bat file

--- serverless/tcwhoosh/deploy.bat
+++ serverless/tcwhoosh/deploy.bat
@@ -1,1 +1,10 @@
+SET currentFolder = %cd%
+FOR /D %%p IN ("C:\Users\%USERNAME%\AppData\Local\UnitedIncome\serverless-python-requirements\Cache\*.*") DO (
+    echo %%p | findstr /r downloadCacheslspyc > NUL
+    if errorlevel 1 (
+        rmdir "%%p" /s /q
+    )
+)
+cd %currentFolder%
+
 call serverless deploy --stage %1 --verbose
